74e6b3f68440dcc1f09f4eb781fb60e5da810f03
slumpthug <gangguchi58@gmail.com>
2022-06-18 00:40:50 +0600

    did task 2

--- task-02/index.js
+++ task-02/index.js
@@ -1,3 +1,11 @@
-function test() {
-    
+let arr = [2,3,4,5] 
+
+function test(number) {
+    return arr.filter(num => number % num === 0) 
 }
+
+console.log(test(36));
+
+console.log(test(55));
+
+console.log(test(333));
